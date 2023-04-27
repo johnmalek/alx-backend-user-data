ec35d1a4d13b2aa072659380cdf6a0346233a1c4
Malek <johnmalek54@gmail.com>
2023-04-27 21:23:04 +0000

    Task 1 and 2

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,6 +4,9 @@ A python module
 """
 from typing import List
 import re
+import logging
+
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
 def filter_datum(
@@ -20,6 +23,49 @@ def filter_datum(
                 field+'=.*?'+separator,
                 field+'='+redaction+separator, message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """
+    Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+         filter values in incoming log records using filter_datum
+        """
+        message = super(RedactingFormatter, self).format(record)
+        redact = filter_datum(
+                self.fields,
+                self.REDACTION,
+                message,
+                self.SEPARATOR)
+        return redact
+
+
+def get_logger() -> logging.Logger:
+    """
+    Returns the logging.Logging object
+    """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    handler = logging.StreamHandler()
+
+    formatter = RedactingFormatter(PII_FIELDS)
+
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    return logger
 
 
 if __name__ == "__main__":
