424cb05b145115a8f54162901ea782265a4e698f
Malek <johnmalek54@gmail.com>
2023-04-27 10:36:17 +0000

    Task 0 correction

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,16 +5,22 @@ A python module
 from typing import List
 import re
 
-def filter_datum(fields: List[str], redaction: str,
-        message: str, separator: str) -> str:
+
+def filter_datum(
+        fields: List[str],
+        redaction: str,
+        message: str,
+        separator: str) -> str:
     """
     function called filter_datum that returns
     the log message obfuscated
     """
     for field in fields:
-        message = re.sub(field+'=.*?'+separator,
+        message = re.sub(
+                field+'=.*?'+separator,
                 field+'='+redaction+separator, message)
     return message
+
 
 if __name__ == "__main__":
     main()
