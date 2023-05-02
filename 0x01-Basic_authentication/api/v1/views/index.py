1553b3b72133f026ec8bf3705b66d8e7b1eac871
Malek <johnmalek54@gmail.com>
2023-05-02 16:44:17 +0000

    Task 1

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -25,3 +25,8 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
+@app_views.route('/unauthorized', strict_slashes=False)
+def unauthorized() -> str:
+    """Unauthorized handler
+    """
+    return abort(401)
