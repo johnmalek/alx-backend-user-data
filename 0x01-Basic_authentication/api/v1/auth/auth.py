c834cafc49465b518cbfa7f62e4a2ca4140b2eee
Malek <johnmalek54@gmail.com>
2023-05-02 18:39:58 +0000

    Task 4 corrections

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -19,6 +19,8 @@ class Auth:
             return True
         elif path in excluded_paths:
             return False
+        else:
+            return False
 
     def authorization_header(self, request=None) -> str:
         """a public method
