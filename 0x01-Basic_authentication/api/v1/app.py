f4350ee9c3bd63fb595feb509d36ce1529f90fa9
Malek <johnmalek54@gmail.com>
2023-05-02 17:05:35 +0000

    Task 2 corrections

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -32,7 +32,7 @@ def unauthorized(error) -> str:
 def forbidden(error) -> str:
     """Handle forbidden error
     """
-    return jsonify("error": "Forbidden"), 403
+    return jsonify({"error": "Forbidden"}), 403
 
 
 if __name__ == "__main__":
