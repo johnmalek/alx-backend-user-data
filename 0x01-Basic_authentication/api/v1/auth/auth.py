bd2dfa99273e5a59b40b42e2b954e299b761eeb8
Malek <johnmalek54@gmail.com>
2023-05-02 17:16:31 +0000

    Task 3

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""A python module
+"""
+from flask import request
+from typing import List
+
+
+class Auth:
+    """Authentication class
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """a public method
+        Return:
+          - False
+        """
+        return False
+
+
+    def authorization_header(self, request=None) -> str:
+        """a public method
+        Returns:
+          - None
+        """
+        return None
+
+    
+    def current_user(self, request=None) -> TypeVar('User'):
+        """a public method
+        Return:
+          - None
+        """
+        return None
