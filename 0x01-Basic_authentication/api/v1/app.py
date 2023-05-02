f9b2b7a015d552f8f695f9885aab4b4684e34d95
Malek <johnmalek54@gmail.com>
2023-05-02 21:25:41 +0000

    Task 5 correctons + task 6

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -5,6 +5,7 @@ Route module for the API
 from os import getenv
 from api.v1.views import app_views
 from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
@@ -14,10 +15,11 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
-
-if auth:
-    auth_instance = Auth()
-    auth = auth_instance
+auth_type = os.getenv("AUTH_TYPE")
+if auth_type == "basic_auth":
+    auth = BasicAuth()
+else:
+    auth = Auth()
 
 
 @app.errorhandler(404)
@@ -42,13 +44,13 @@ def forbidden(error) -> str:
 
 
 @app.before_request
-def before_request(auth) -> str:
+def before_request() -> str:
     """handle before_request
     """
     path_list = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
     if auth is None:
         return
-    elif auth.require_auth(request.path) is not in path_list:
+    elif auth.require_auth(request.path, path_list) not in path_list:
         return
     elif auth.authorization_header(request) is None:
         return abort(401)
