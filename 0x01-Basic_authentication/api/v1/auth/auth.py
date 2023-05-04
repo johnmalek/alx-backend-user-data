9b00e4b3c39471273074e6a65e1e6cd53d787d8b
Malek <johnmalek54@gmail.com>
2023-05-04 09:30:36 +0000

    rest of the files

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -15,7 +15,7 @@ class Auth:
         """
         if path is None:
             return True
-        elif excluded_paths is None or len(excluded_paths) == 0:
+        elif excluded_paths is None or excluded_paths == []:
             return True
         elif path in excluded_paths:
             return False
