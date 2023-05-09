ba25450021147a7882299ccc7323e6972895f271
Malek <johnmalek54@gmail.com>
2023-05-09 15:04:11 +0000

    Task 0

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""A python module
+"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Integer, String, Column
+
+Base = declarative_base()
+
+
+class User(Base):
+    """An Sqlalchemy model
+    """
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    session_id = Column(String)
+    reset_token = Column(String)
